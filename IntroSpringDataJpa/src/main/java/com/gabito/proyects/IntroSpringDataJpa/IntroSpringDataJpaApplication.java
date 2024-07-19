b83a95f5769b654b5088db6e7d7545dffe6b8469
DanielGabGon <dan.gabito@gmail.com>
2024-07-19 15:50:30 -0600

    Logro: Creación proyecto y creación de repositorio GIT

---
+++ IntroSpringDataJpa/src/main/java/com/gabito/proyects/IntroSpringDataJpa/IntroSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.gabito.proyects.IntroSpringDataJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringDataJpaApplication.class, args);
+	}
+
+}
